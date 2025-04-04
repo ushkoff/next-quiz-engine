4a0494d4a31df802afcf6712c1adb37b099ae6ed
ushkoff <lol454ushkbeavise@gmail.com>
2025-04-04 15:33:22 +0200

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,24 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const googleConfig = compat.extends('google').map((config) => {
+  if (config.rules) {
+    delete config.rules['valid-jsdoc'];
+  }
+  return config;
+});
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...googleConfig,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('prettier'),
+  {
+    rules: {
+      'require-jsdoc': 'off',
+      'new-cap': 'off',
+      quotes: ['error', 'single'],
+    },
+  },
 ];
 
 export default eslintConfig;
